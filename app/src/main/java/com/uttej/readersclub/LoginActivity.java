74763b29d1068dba5511c01e3a2d3ee791a52356
Uttejkumar <uttej60@gmail.com>
2018-03-23 21:50:50 +0530

    included central activity with bottom navigation

--- app/src/main/java/com/uttej/readersclub/LoginActivity.java
+++ app/src/main/java/com/uttej/readersclub/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.uttej.readersclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button skip_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        skip_button  = (Button) findViewById(R.id.skip);
+        skip_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent skip_login = new Intent(LoginActivity.this, CentralActivity.class);
+                startActivity(skip_login);
+            }
+        });
     }
 }
